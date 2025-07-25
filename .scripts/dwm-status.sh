10759179a7a42742b69f6db7873077ff394915fa
Franek Szczybelski <franekszczybelski@gmail.com>
2025-07-25 16:53:57 +0200

    fixes

--- .scripts/dwm-status.sh
+++ .scripts/dwm-status.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+getVolume() {
+  if pactl get-sink-mute @DEFAULT_SINK@ | grep -q "yes"; then
+    echo "Muted"
+  else
+    pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]\+%' | head -1
+  fi
+}
+
 while true; do
-  xsetroot -name "$(date '+%m-%d %H:%M')"
-  sleep 1m
+  xsetroot -name "$(getVolume) | $(date '+%m-%d %H:%M')"
+  sleep 1
 done
