d04e872a22545a0bb005aaf2f7d12bd3c6ba020a
Franek Szczybelski <franekszczybelski@gmail.com>
2025-08-10 10:11:00 +0200

    small changes

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -82,7 +82,7 @@ return{
 
       lspconfig["clangd"].setup({
         cmd = {
-          "/usr/bin/clangd",
+          "/usr/lib/llvm/20/bin/clangd",
           "--background-index",
           "--pch-storage=memory",
           "--all-scopes-completion",
